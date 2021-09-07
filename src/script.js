4edcc830558e19677ed567acd0fdd664b0cdfb61
Ulyana <ulyana.sikinski@gmail.com>
2021-09-07 14:37:15 +0500

    some correcting

---
+++ src/script.js
@@ -0,0 +1,39 @@
+const toggleDisplay = function (el) {
+    el.style.display = (el.style.display !== 'block') ? 'block' : 'none';
+}
+const changeIcon = function (el, class1, class2) {
+    if(el.classList.contains(class1)) {
+        el.classList.toggle(class2);
+    } else {
+        el.classList.toggle(class2);
+    }
+}
+const overlay = document.querySelector('.overlay');
+
+// header
+
+const headerPopupBtn = document.querySelector('.header__open-popup-btn');
+const headerPopupBtnIcon = document.querySelector('.header__open-popup-btn i');
+const headerPopupEl = document.querySelector('.header__content .header__wrapper-popup');
+
+headerPopupBtn.addEventListener('click', function () {
+    toggleDisplay(headerPopupEl);
+    changeIcon(headerPopupBtnIcon, 'fa-chevron-down', 'fa-chevron-up');
+})
+
+// nav
+const navSearchInput = document.querySelector('nav .nav__content .nav__buttons .nav__search-input');
+const navSearchBtn = document.querySelector('nav .nav__content .nav__buttons .nav__search-btn');
+
+// main
+const filterFooterBlock = document.querySelector('main .main__posts-section .main__header-streams .main__search-filter');
+const openFilterBtn = document.querySelector('main .main__posts-section .main__header-streams .main__search-filter button');
+const openFilterBtnIcon = document.querySelector('main .main__posts-section .main__header-streams .main__search-filter button i');
+const filterSearchPopup = document.querySelector('main .main__posts-section  .main__search-filter-block');
+
+
+openFilterBtn.addEventListener('click', function() {
+    filterFooterBlock.style.borderBottom = (filterFooterBlock.style.borderBottom !== '1px solid rgba(0, 0, 0, 0.3)') ? '1px solid rgba(0, 0, 0, 0.3)' : 'inherit';
+    changeIcon(openFilterBtnIcon, 'fa-chevron-down', 'fa-chevron-up');
+    toggleDisplay(filterSearchPopup);
+});
